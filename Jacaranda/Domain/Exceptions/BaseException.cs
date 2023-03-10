a0762a1fb23634d73b2c55def33f59e1f9917b12
barros <mdbf42@gmail.com>
2023-03-10 00:20:49 +0000

    Fix @ BaseException

--- Jacaranda/Domain/Exceptions/BaseException.cs
+++ Jacaranda/Domain/Exceptions/BaseException.cs
@@ -10,7 +10,7 @@ namespace Jacaranda.Domain.Exceptions
 
         protected BaseException(string Code, string Message)
         {
-            Data.Add("Code", "IPE-" + Code);
+            Data.Add("Code", "JAC-" + Code);
             Data.Add("Message", Message);
             Data.Add("ShortMessage", Message.Replace(" ", string.Empty));
         }
